91cb8a745b9d31973e15ed82e4b3137e4d77c62b
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2021-06-22 13:18:01 -0400

    [Chapter3] - config docker

--- typescript/routes/specifications.routes.ts
+++ typescript/routes/specifications.routes.ts
@@ -6,7 +6,7 @@
 
 import { Router } from "express";
 
-import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+import { createSpecificationController } from "../src/modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
