ce89cbee3b5cbbdcf80229b906696cb6de43bfec
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2021-07-08 10:05:14 -0400

    [Chapter4] - updated paths and created Cars migration

--- typescript/src/shared/infra/http/server.ts
+++ typescript/src/shared/infra/http/server.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "../database";
+import "../database/typeorm";
 import "../../container";
 import { AppError } from "@shared/errors/AppError";
 
