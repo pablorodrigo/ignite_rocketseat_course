fb3e666c6577448444a7cbf1eebc0431ec4b0686
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2021-08-09 16:34:14 -0400

    [Chapter6] - update typeorm config for project under production

--- typescript/src/shared/infra/database/typeorm/index.ts
+++ typescript/src/shared/infra/database/typeorm/index.ts
@@ -20,12 +20,27 @@ getConnectionOptions().then((options) => {
 });
 */
 
+/*
 export default async (host = "database"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
       host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "rentx_test"
+          : defaultOptions.database,
+    })
+  );
+};
+*/
+
+export default async (): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
