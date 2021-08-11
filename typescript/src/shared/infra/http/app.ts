8d5946a48b3139beac4789ec3f19135bb929f2ff
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2021-08-11 10:17:19 -0400

    [Chapter6] - configured cors

--- typescript/src/shared/infra/http/app.ts
+++ typescript/src/shared/infra/http/app.ts
@@ -6,6 +6,7 @@
 
 import "reflect-metadata"; // to fix error Error: tsyringe requires a reflect polyfill. Please add 'import "reflect-metadata"' to the top of your entry point.
 import "dotenv/config";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
@@ -27,6 +28,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
